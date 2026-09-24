c2f74f9f0411b27aa3cf0e016c19bd6b80402e2a
Sakshi Harsora <you@example.com>
2026-09-24 21:59:53 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
+    "https://ml-project-mauve-eta.vercel.app",
 ],
     allow_credentials=True,
     allow_methods=["*"],
